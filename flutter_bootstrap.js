bc20d2bd028e7257287e9fa9283d11108a7f8ca3
zawxtut <zawxtutaung@gmail.com>
2024-08-09 14:40:06 +0700

    up

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2949547336"
+    serviceWorkerVersion: "2802742174"
   }
 });
